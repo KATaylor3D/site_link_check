27e1884626072d2a8dae23ca00e1ed5b5a20befb
Kyle Taylor <KATaylor3D@gmail.com>
2020-12-31 17:15:29 -0600

    Refactored added save, read and comprehension functions

--- check_site_links.py
+++ check_site_links.py
@@ -3,95 +3,166 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
-sleep_time = 0.2
+sleep_time = 3
 
-def get_site_links(url):
-    site = requests.get(url).text
-    soup = BeautifulSoup(site, 'lxml')
-    links = []
-    for a_tag in soup.findAll("a"):
-        href = a_tag.attrs.get("href")
-        links.append(href)
-    return list(set(links))
+def build_starting_tuple(url):
+    url = url
+    parent = url
+    text = 'OG url'
+    status_code = 200
+    return (url, parent, text, status_code)
 
-def fix_links(urls, site):
-    filtered_links = []
-    for url in urls:
+def get_links_parents_and_texts(url_tup):
+    url, parent, text, status_code  = url_tup
+    try:
+        site = requests.get(url, timeout=10).text
+        soup = BeautifulSoup(site, 'lxml')
+        url_tup = set()
+        for a_tag in soup.findAll("a"):
+            href = a_tag.attrs.get("href")
+            text = a_tag.text
+            url_tup.add((href, url, text))
+        return url_tup
+    except requests.InvalidHeader as err:
+        print(err)
+        print('Invalid Header. Failed to get_links_with_parent for url: {url} child of {parent}')
+    except requests.RequestException as err:
+        print(err)
+        print('Failed to get_links_with_parent for url: {url} child of {parent}')
+        return (url, parent, text)
+
+def mend_links(url_tup, site):
+    mended_links = set()
+    for url, parent, text in url_tup:
         if url == None:
-            filtered_links.append(site)
-        elif url.startswith('mailto:') or url.startswith('tel:'):
-            filtered_links.append(site)
+            mended_links.add((site, site, 'OG url'))
+        elif url.startswith('mailto:') or url.startswith('tel:') or url.startswith('javascript:'):
+            mended_links.add((site, site, 'OG url'))
+        elif '?EventViewMode' in url:
+            mended_links.add((site, site, 'OG url'))
         elif url.startswith('https://') or url.startswith('http://'):
-            filtered_links.append(url)
+            mended_links.add((url, parent, text))
         elif url.startswith('/'):
-            filtered_links.append(site + url[1:])
+            mended_links.add((site + url[1:], parent, text))
         else:
-            filtered_links.append(site + url)
-    return list(set(filtered_links))
+            mended_links.add((site + url, parent, text))
+    return mended_links
 
-def links_in_site(urls, site):
-    links = []
-    for url in urls:
+def add_status_code(url_tup):
+    url, parent, text = url_tup
+    while True:
         try:
-            if url.startswith(site):
-                links.append(url)
-        except:
-            pass
-    return links
-
-def links_with_status_codes(urls):
-    links_codes = []
-    for url in urls:
-        while True:
-            sleep(sleep_time)
-            try:
-                status_code = requests.get(url).status_code
-            except:
-                status_code = 0
-            if status_code != 429:
+            status_code = requests.get(url).status_code
+            if status_code == 429:
+                print('Too many requests in an amount of time')
+                print('Waiting {sleep_time} second(s) and will try again')
+                sleep(sleep_time)
+                continue
+            elif status_code == 200:
                 break
-        links_codes.append((url, status_code))
-    return links_codes
+            else:
+                break
+        except requests.RequestException as err:
+            print(err)
+            print(f"Failed getting status code for {url_tup}")
+            status_code = 0
+            break
+    return (url, parent, text, status_code)
 
-def filter_links(urls, site):
-    fixed_links = fix_links(urls, site)
-    site_links = links_in_site(fixed_links, site)
-    links_and_status_codes = links_with_status_codes(site_links)
-    return links_and_status_codes
+def is_link_in_site(url_tup, site):
+    url, *b = url_tup
+    return url.startswith(site)
+
+def list_to_examine(url_tup):
+    comp = []
+    for url, *b in url_tup:
+        comp.append(url)
+    return comp
 
 
-
-def main(site):
-    visited_links = [(f'{site}', 200)]
-    all_links = []
-    links = get_site_links(site)
-    filtered_links = filter_links(links, site)
-    all_links = filtered_links
-    all_links.remove((f'{site}', 200))
+def search_site_for_links_status(site):
+    original_link = build_starting_tuple(site)
+    visited_links = set()
+    all_links = set()
+    all_links.add(original_link)
     scanning = True
     while scanning:
-        if all_links != []:
-            current_link = all_links[0]
-            os.system('cls')
-            print('Current link: ', current_link)
-            print('Visited links: ', visited_links)
-            print('Number of Unvisited links: ', len(all_links))
-            print('Number of Visited links: ', len(visited_links))
-            visited_links.append(current_link)
-            all_links.remove(current_link)
-            links = get_site_links(current_link[0])
-            filtered_links = filter_links(links, site)
-            for filtered_link in filtered_links:
-                if filtered_link in visited_links:
-                    print('already visited')
-                else:
-                    all_links.append(filtered_link)
-            sleep(.75)
-        else:
+        if all_links == set():
             scanning = False
-    print(visited_links)
+        else:
+            os.system('cls')
+            print(f"{len(all_links)} site(s) to visit")
+            print(f"{len(visited_links)} site(s) visited")
+            url = all_links.pop()
+            print(f"Visiting {url}...")
+            print(f'    Removed {url} from all_links')
+            visited_links.add(url)
+            print(f'    Added {url} to visited_links')
+            if is_link_in_site(url, site) and url[3] == 200 and not url[0].endswith('.pdf'):
+                links = get_links_parents_and_texts(url)
+                links = mend_links(links, site)
+                for link in links:
+                    if link[0] not in list_to_examine(visited_links):
+                        full_link = add_status_code(link)
+                        all_links.add(full_link)
+                        print(f"    Added {full_link} to all_links")
+                    else:
+                        print(f"    Skipped {link} to all_links")
+    print('Saving to file')
+    with open('log.txt', 'w') as file:
+        for link in visited_links:
+            file.write(str(link) + '\n')
+    print('Complete')
+
+def get_url_tups_from_file(filename):
+    url_tups = set()
+    with open(filename, 'r') as file:
+        content = file.read()
+    seperate_tups = content.split('\n')[:-1]
+    for tup in seperate_tups:
+        trimmed_str = tup[1:-1]
+        link, parent, *end = trimmed_str.split(", ")
+        link = link[1:-1]
+        parent = parent[1:-1]
+        end = str(end)[1:-1]
+        text, status_code = end.rsplit(", ", 1)
+        text = text[2:-2]
+        status_code = int(status_code[1:-1])
+        url_tup = (link, parent, text, status_code)
+        url_tups.add(url_tup)
+    return url_tups
+
+def get_problem_links(url_tups):
+    problem_links = set()
+    for url_tup in url_tups:
+        if url_tup[3] != 200 and url_tup[3] != 404:
+            problem_links.add(url_tup)
+    return problem_links
+
+def get_broken_links(url_tups):
+    broken_links = set()
+    for url_tup in url_tups:
+        if url_tup[3] == 404:
+            broken_links.add(url_tup)
+    return broken_links
+
+def interface():
+    search = input('Website to check: ')
+
+def save_report(url_tups):
+    problem_links = get_problem_links(url_tups)
+    broken_links = get_broken_links(url_tups)
+    with open('report.txt', 'w') as file:
+        for link in broken_links:
+            file.write(str(link) + '\n')
+        for link in problem_links:
+            file.write(str(link) + '\n')
+        file.write(f"There are {len(broken_links)} broken links and {len(problem_links)} problem links")
 
 if __name__ == '__main__':
-    os.system('cls')
-    site = input('Website to check: ')
-    main(site)
+    # os.system('cls')
+    # site = 'https://iaqf.org/'
+    # # input('Website to check: ')
+    # main(site)
+    url_tups = get_url_tups_from_file('relog.txt')
+    save_report(url_tups)
